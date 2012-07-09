381ffc86550dca7f7b7aaceb79f667e38d0807e9
Atul Varma <varmaa@gmail.com>
2012-07-09 12:17:24 -0400

    uploaded files are converted to ogg and mp3.

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 var express = require('express'),
     fs = require('fs'),
+    spawn = require('child_process').spawn,
     app = express.createServer();
 
-var STATIC_DIR = __dirname + '/static';
+var STATIC_DIR = __dirname + '/static',
+    FFMPEG_VORBIS = 'ffmpeg',
+    FFMPEG_MP3 = '/Applications/ffmpegX.app/Contents/Resources/ffmpeg';
 
 app.post('/audio', function(req, res) {
   var name = req.get('name', 'UNKNOWN'),
-      outfile = fs.createWriteStream(STATIC_DIR + '/outfile.wav');
+      basefilename = STATIC_DIR + '/outfile',
+      wavfilename = basefilename + '.wav',
+      outfile = fs.createWriteStream(wavfilename);
 
+  console.log("LENGTH", req.header('content-length'));
   req.pipe(outfile);
+  req.on('data', function(chunk) {
+    console.log("DATA", chunk.length);
+  });
+  outfile.on('close', function() {
+    console.log("OUTFILE CLOSED, converting to ogggg.");
+    var baseArgs = ['-i', wavfilename, '-y'];
+    var mp3 = spawn(FFMPEG_MP3, baseArgs.concat([
+      '-acodec', 'mp3',
+      '-ab', '64k',
+      basefilename + '.mp3'
+    ]));
+    var vorbis = spawn(FFMPEG_VORBIS, baseArgs.concat([
+      '-acodec', 'libvorbis',
+      '-ac', '2',
+      basefilename + '.ogg'
+    ]));
+    mp3.on('exit', function(code) {
+      console.log('mp3 exited with code', code);
+    });
+    vorbis.on('exit', function(code) {
+      console.log('vorbis exited with code', code);
+    });
+    console.log('ok');
+  });
   res.send("THANKS YO");
 });
 
